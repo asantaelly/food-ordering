0ef8e996a508ab4a53f75acbc998d2a402f234ee
wi11z <9alile1@gmail.com>
2020-05-18 20:59:22 +0300

    starting an app

--- food_ordering/settings.py
+++ food_ordering/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'suppliers.apps.SuppliersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'food_ordering.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'foodie_db',
+        'USER': 'willz',
+        'PASSWORD': 'iamgroot',
+        'HOST': 'localhost'
     }
 }
 
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'food_delivery.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'food_ordering.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
