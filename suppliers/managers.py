94e0e8b2a082b357e799e59ae08eb4dddf15f2cf
wi11z <9alile1@gmail.com>
2020-05-19 20:45:05 +0300

    Added custom user model

---
+++ suppliers/managers.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    """
+     Database Table Manager for users table, to create and store user data
+    """
+    def _create_user(self, email=None, password=None, is_superuser=False, is_staff=False, is_active=False):
+        
+        if email is None:
+            raise ValueError("Email is required")
+        if password is None:
+            raise ValueError("Password is required")
+
+        user = self.model(
+            email = self.normalize_email(email),
+            is_superuser = is_superuser,
+            is_staff = is_staff,
+            is_active = is_active,
+        )
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_user(self, email=None, password=None, **custom_fields):
+        user = self._create_user(email, password, is_active=False, is_staff=False, is_superuser=False)
+        user.role = custom_fields['role']
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **custom_fields):
+        user = self._create_user(email, password, is_superuser=True, is_staff=True, is_active=True)
+        user.save()
+        return user
+
--- suppliers/models.py
+++ suppliers/models.py
@@ -1,4 +1,42 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser
+
+from .managers import CustomUserManager
 
 # Create your models here.
+class CustomUser(AbstractBaseUser):
+    """
+        User Data Table, Customized from the default django user table
+    """
+    normal_user = 'U'
+    supplier = 'S'
+    role_choices = [
+        (normal_user, 'User'),
+        (supplier, 'Supplier'),
+    ]
+
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=1, choices=role_choices, default=normal_user, blank=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+    last_login = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['role']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+    def get_username(self):
+        return self.email
