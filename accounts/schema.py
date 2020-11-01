59144ee92bdbc4e349bfa5a1abfb0129929a36ed
wi11z <9alile1@gmail.com>
2020-11-01 10:38:29 +0300

    Create user Mutation

--- accounts/schema.py
+++ accounts/schema.py
@@ -16,18 +16,30 @@ class Query(graphene.ObjectType):
     all_users = graphene.List(CustomerUserType)
     user_by_id = graphene.Field(CustomerUserType, id=graphene.String())
 
-    def resolve_all_users(root, info, **kwargs):
+    def resolve_all_users(self, info, **kwargs):
         return CustomUser.objects.all()
 
-    def resolve_user_by_id(root, info, id):
+    def resolve_user_by_id(self, info, id):
         return CustomUser.objects.get(pk=id)
 
 
-class AccountMutation(DjangoModelFormMutation):
-    custom_user = graphene.Field(CustomerUserType)
+class CreateUser(DjangoModelFormMutation):
 
     class Meta:
         form_class = RegisterForm
 
+    custom_user = graphene.Field(CustomerUserType)
+
+    # def mutate(self, info, form_class=None):
+    #     custom_user = CustomUser(
+    #         first_name=form_class.first_name,
+    #         last_name=form_class.last_name,
+    #         email=form_class.email,
+    #     )
+
+
+        # return CreateUser(custom_user=custom_user)
+
+
 class Mutation(graphene.ObjectType):
-    create_user = AccountMutation.Field()
+    create_user = CreateUser.Field()
--- menu/schema.py
+++ menu/schema.py
@@ -22,11 +22,11 @@ class Query(graphene.ObjectType):
     all_menus = graphene.List(MenuType)
     menu_by_id = graphene.Field(MenuType, id=graphene.String())
 
-    def resolve_all_menus(root, info, **kwargs):
+    def resolve_all_menus(self, info, **kwargs):
         # Querying a list
         return Menu.objects.all()
 
-    def resolve_menu_by_id(root, info, id):
+    def resolve_menu_by_id(self, info, id):
         # Querying a single menu
         return Menu.objects.get(pk=id)
 
